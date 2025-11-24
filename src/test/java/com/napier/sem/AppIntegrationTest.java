4f731ef93d36cf4f5810d115e045a8e9a5bb2aef
Kai Bryan <bryan.07306@gmail.com>
2025-11-24 23:05:21 +0000

    Updated for deployment

--- src/test/java/com/napier/sem/AppIntegrationTest.java
+++ src/test/java/com/napier/sem/AppIntegrationTest.java
@@ -36,4 +36,18 @@ public class AppIntegrationTest
         assertEquals(dept.dept_no, "d007");
         assertEquals(dept.manager.emp_no, 111035);
     }
+
+    @Test
+    void testAddEmployee()
+    {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Kevin");
+        assertEquals(emp.last_name, "Chalmers");
+    }
 }
